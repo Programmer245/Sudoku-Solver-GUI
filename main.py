7ba4e9abbf5a57aa289bdcc0a6505f9a0646941c
Antony  Brun [Student] <bruna15@s.padua.vic.edu.au>
2020-06-02 15:02:49 +1000

    Added SudokuGrid class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import tkinter
 import copy # Used for creating copies of variables instead of instances
 
-solutions = []
+solutions = [] # Stores all the solved grids
 
 grid = [ # Grid representing the 8x8 sudoku grid
 [1,0,6,0,0,2,0,0,0], 
@@ -17,57 +17,171 @@ grid = [ # Grid representing the 8x8 sudoku grid
 [9,0,0,8,7,4,2,1,0]
 ]
 
-def possible(grid, x, y, n):
-    '''Takes in the grid, x position, y position, and value of a possible number
+class SudokuGrid():
+    'Creates the Sudoku Grid'
 
-    Returns True or False if a number can fit in a specific position in the grid'''
+    def __init__(self, parent): # Parent is the main window
+        self.parent = parent
+        self.margin = 20 # margin of the sudoku board
+        self.side = 50 # side of each square in the grid
+        self.width = self.height = (self.margin*2) + (self.side*9) # Defines the width and height of the canvas
+        self.fonttype = ('Helvetica', 20, 'bold') # Font size in canvas grid
+        
+        self.row = -1  # Currently selected cell row and colunm
+        self.col = -1
 
-    # Checks row
-    for position in grid[y]:
-        if position == n:
-            return False
+        self.__initUI() # Creates the other widgets
+        self.__solve_grid(grid) ################################################################################################################################################################
+        self.__display_solutions() 
+        exit()
 
-    # Checks column
-    for row in grid:
-        if row[x] == n: 
-            return False
+    def __initUI(self):
+        'Sets up elements in the GUI'
 
-    # Checks square
-    ranges = [range(0,3), range(3,6), range(6,9)] # Possible grid ranges
+        self.frame = tkinter.Frame(self.parent) # Creates a frame inside the parent 
+        self.canvas = tkinter.Canvas(self.frame, bg='lightblue', width=self.width, height=self.height) # Sudoku grid
 
-    xrange = None # Stores the ranges that x and y are in
-    yrange = None
+        self.frame.pack()
+        self.canvas.pack()
 
-    for possible_range in ranges:
-        if x in possible_range:
-            xrange = possible_range # If x fits in the range, the range is stored
-        if y in possible_range:
-            yrange = possible_range # If y fits in the range, the range is stored
+        self.__draw_grid() # Draws the grid
 
-    for row in grid[yrange[0]:yrange[-1]+1]:
-        for position in row[xrange[0]:xrange[-1]+1]:
-            if position == n: # Checks every position in the square
+        self.canvas.bind('<Button-1>', self.__cell_clicked) # Binds left click to selecting a cell
+        self.parent.bind('<Key>', self.__key_pressed) # Binds key pressed to entering a key; must be binded to root
+
+    def __draw_grid(self):
+        'Draws the actual grid'
+
+        for i in range(10):
+            if i % 3 == 0: # Every 3 lines switches to black
+                color = 'black'
+            else:
+                color = 'grey'
+            
+            # Vertical lines
+            x0 = self.margin + (i*self.side)
+            y0 = self.margin
+            x1 = self.margin + (i*self.side)
+            y1 = self.height - self.margin
+            self.canvas.create_line(x0,y0,x1,y1, fill=color)
+
+            # Horizontal lines
+            x0 = self.margin
+            y0 = self.margin + (i*self.side)
+            x1 = self.height - self.margin
+            y1 = self.margin + (i*self.side)
+            self.canvas.create_line(x0,y0,x1,y1, fill=color)
+
+    def __cell_clicked(self, event):
+        x, y = event.x, event.y # Finds the x and y coordinate of the click
+        print(f'Clicked at {x},{y}') ################################################################################################################################################################
+
+        if (self.margin < x < self.width - self.margin) and (self.margin < y < self.height - self.margin): # Checks that the click is inside the grid
+            row, col = (y-self.margin)//self.side, (x-self.margin)//self.side # Calculates what row and column the cursor is in
+            print(f'Clicked at row {row}, column {col}') ################################################################################################################################################################
+
+            if (row, col) == (self.row, self.col): # If cells is already selected, deselect it
+                self.row, self.col = -1, -1
+
+            else: # If it is not selected, select it
+                self.row, self.col = row, col
+
+            self.__draw_cursor() # Handles the box selection
+
+        else: # If the user clicks outself.side the canvas
+            self.row, self.col = -1, -1 # Resets the currently selected cell row and colunm
+            self.canvas.delete('cursor') # Deletes the previous cursor
+
+    def __draw_cursor(self):
+        self.canvas.delete('cursor') # Deletes the previous cursor
+
+        if (self.row, self.col) != (-1, -1): # Checks that a box has not been deselected (-1, -1 only occur if deselected)
+            x0 = self.margin + self.col*self.side # Defines the boundaries of the rectangle selection cursor
+            y0 = self.margin + self.row*self.side
+            x1 = self.margin + (self.col+1)*self.side 
+            y1 = self.margin + (self.row+1)*self.side 
+            self.canvas.create_rectangle(x0, y0, x1, y1, tags='cursor', outline='green', width=3)
+
+    def __key_pressed(self, event):
+        if event.char.isnumeric(): # Only puts number in if it is numberic
+            print(self.row, self.col)
+            x = self.margin + self.side*self.col + self.side/2 # Finds x and y coords of the centre of the selected square
+            y = self.margin + self.side*self.row + self.side/2
+            
+            tag = f'{x}{y}' # Create a tag depending on the x and y coords of the centre of the selected square
+            
+            self.canvas.delete(tag) # Deletes previous 
+            self.canvas.create_text(x, y, text=event.char, tags=tag, fill='black', font=self.fonttype) # Places a number on the screen
+
+    def __solve_grid(self, grid):
+        '''Solves the grid and stores the solutions in solutions list
+        
+        Displays every iteration of the grid'''
+        
+        for ypos, row in enumerate(grid): # Goes through each row in the column
+            for xpos, position in enumerate(row): # Goes through each position in the row
+                if position == 0: # Position must be empty
+                    for num in range(1,10): # Tries all numbers from 1 to 9
+                        if self.__possible(grid, xpos, ypos, num): # Check if the number is a possible
+                            grid[ypos][xpos] = num # Puts possible number in empty space
+                            self.__solve_grid(grid) # Keeps solving
+                            grid[ypos][xpos] = 0 # If program reaches here, no further numbers can be put into the grid and the square is reset
+                    
+                    return False # No possible solution has been found for an empty position; exits functions
+
+        # If program reaches this point, there are no more empty spaces in the grid and a solution has been found
+
+        deepcopy_grid = copy.deepcopy(grid) # A copy of the original grid is made
+        solutions.append(deepcopy_grid)
+    
+    def __possible(self, grid, x, y, n):
+        '''Takes in the grid, x position, y position, and value of a possible number
+
+        Returns True or False if a number can fit in a specific position in the grid'''
+
+        # Checks row
+        for position in grid[y]:
+            if position == n:
                 return False
 
-    return True # No doubles detected
+        # Checks column
+        for row in grid:
+            if row[x] == n: 
+                return False
 
-def solve(grid):
-    '''Yields each solution in order'''
-    
-    for ypos, row in enumerate(grid): # Goes through each row in the column
-        for xpos, position in enumerate(row): # Goes through each position in the row
-            if position == 0: # Position must be empty
-                for num in range(1,10): # Tries all numbers from 1 to 9
-                    if possible(grid, xpos, ypos, num): # Check if the number is a possible
-                        grid[ypos][xpos] = num # Puts possible number in empty space
-                        solve(grid) # Keeps solving
-                        grid[ypos][xpos] = 0 # If program reaches here, no further numbers can be put into the grid and the square is reset
-                
-                return False # No possible solution has been found for an empty position; exits functions
+        # Checks square
+        ranges = [range(0,3), range(3,6), range(6,9)] # Possible grid ranges
 
-    # If program reaches this point, there are no more empty spaces in the grid and a solution has been found
+        xrange = None # Stores the ranges that x and y are in
+        yrange = None
 
-    deepcopy_grid = copy.deepcopy(grid) # A copy of the original grid is made
-    solutions.append(deepcopy_grid)
+        for possible_range in ranges:
+            if x in possible_range:
+                xrange = possible_range # If x fits in the range, the range is stored
+            if y in possible_range:
+                yrange = possible_range # If y fits in the range, the range is stored
 
-solve(grid)
+        for row in grid[yrange[0]:yrange[-1]+1]:
+            for position in row[xrange[0]:xrange[-1]+1]:
+                if position == n: # Checks every position in the square
+                    return False
+
+        return True # No doubles detected
+
+    def __display_solutions(self):
+        '''Formats and displays all found solutions''' 
+
+        print(f'\n-----------------------{len(solutions)} Solutions Found.-----------------------')
+        
+        for grid in solutions: # Prints out each solution
+            print('')
+            for row in grid:
+                print(row)
+            
+root = tkinter.Tk() # Defines the main window
+root.title('Testing Grid')
+root.resizable('False', 'False')
+
+sudokugrid = SudokuGrid(root) # Interactive Sudoku Grid is created
+
+root.mainloop()
