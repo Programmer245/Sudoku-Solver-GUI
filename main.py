69c8920d8c6862282558ba88bd6573d1c382483c
Antony <brunantony24@gmail.com>
2020-06-04 22:06:43 +1000

    User grid stored as self.grid attribute

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import tkinter
 import copy # Used for creating copies of variables instead of instances
 
+# To retrieve the text from an item with object ID I on a canvas C, call C.itemcget(I, 'text').
+# To replace the text in an item with object ID I on a canvas C with the text from a string S, call C.itemconfigure(I, text=S). 
+
 # Example grid
 # [[1,0,6,0,0,2,0,0,0], 
 # [0,5,0,0,0,6,0,9,1],
@@ -20,19 +23,8 @@ class SudokuGrid():
 
     def __init__(self, parent): # Parent is the main window
         self.parent = parent
-        self.solutions = [] # Stores all the solved grids
 
-        self.grid = [ # Grid representing the 8x8 sudoku grid
-        [1,0,6,0,0,2,0,0,0], 
-        [0,5,0,0,0,6,0,9,1],
-        [0,0,9,5,0,1,4,6,2],
-        [0,3,7,9,0,5,0,0,0],
-        [5,8,1,0,2,7,9,0,0],
-        [0,0,0,4,0,8,1,5,7],
-        [0,0,0,2,6,0,5,4,0],
-        [0,0,4,1,5,0,6,0,9],
-        [9,0,0,8,7,4,2,1,0]
-        ]
+        self.solutions = [] # Stores all the solved grids
 
         self.margin = 20 # Margin size of the sudoku board
         self.side = 50 # Side length of each square in the grid
@@ -48,7 +40,7 @@ class SudokuGrid():
         'Initiates the elements in the grid'
 
         self.frame = tkinter.Frame(self.parent) # Creates a frame inside the parent 
-        self.button = tkinter.Button(self.parent, text='Testing', command=self.__testing) ################################################################################################################################################################
+        self.button = tkinter.Button(self.parent, text='Testing', command=self.__start) ################################################################################################################################################################
         self.canvas = tkinter.Canvas(self.frame, bg='lightblue', width=self.width, height=self.height) # Sudoku grid
 
         self.frame.pack()
@@ -83,14 +75,12 @@ class SudokuGrid():
             y1 = self.margin + (i*self.side)
             self.canvas.create_line(x0,y0,x1,y1, fill=color)
 
-            self.__display_numbers() # Sets each number from self.grid into the grid ################################################################################################################################################################
-
-    def __display_numbers(self, x=None, y=None, n=None): ################################################################################################################################################################
-        '''Represents each number on the grid
+    def __display_numbers(self, x, y, n): ################################################################################################################################################################
+        '''Displays a given number on the grid
 
         Takes in x position, y position, and value of the number'''
 
-        if not x: # If x is False than no arguments have been passed in 
+        pass
 
     def __cell_clicked(self, event):
         '''Handles mouse events
@@ -126,7 +116,7 @@ class SudokuGrid():
             y0 = self.margin + self.row*self.side
             x1 = self.margin + (self.col+1)*self.side 
             y1 = self.margin + (self.row+1)*self.side 
-            self.canvas.create_rectangle(x0, y0, x1, y1, tags='cursor', outline='green', width=3)
+            self.canvas.create_rectangle(x0, y0, x1, y1, tags='cursor', outline='green', width=3) # Creates the cursor
 
     def __key_pressed(self, event):
         '''Handles keyboard events
@@ -134,35 +124,35 @@ class SudokuGrid():
         Takes event as argument'''
 
         if event.char.isnumeric(): # Only puts number in if it is numberic
-            print(self.row, self.col)
-            x = self.margin + self.side*self.col + self.side/2 # Finds x and y coords of the centre of the selected square
-            y = self.margin + self.side*self.row + self.side/2
+            x = round(self.margin + self.side*self.col + self.side/2) # Finds x and y coords of the centre of the selected square
+            y = round(self.margin + self.side*self.row + self.side/2) # Coordinates are rounded to nearest integer
             
-            tag = f'{x}{y}' # Create a tag depending on the x and y coords of the centre of the selected square
+            tag = (self.row,self.col) # Create a tag from 00 to 88 representing the x and y coords of the centre of the selected square
+            print(f'Tag: {tag}') 
             
             self.canvas.delete(tag) # Deletes previous 
-            self.canvas.create_text(x, y, text=event.char, tags=tag, fill='black', font=self.fonttype) # Places a number on the screen
+            self.canvas.create_text(x, y, text=event.char, tags=(tag,), fill='black', font=self.fonttype) # Places a number on the screen with tagged position
+            # tags argument should be a tuple or string
 
     def __solve_grid(self):
-        'Solves the grid and stores each solution as a list in solutions list'
+        'Solves the grid and stores each solution as a list in solutions list. Displays each iteration of the solving algorithm'
         
-        for ypos, row in enumerate(self.grid): # Goes through each row in the column
+        for ypos, row in enumerate(self.grid): # Goes through each row in the grid
             for xpos, position in enumerate(row): # Goes through each position in the row
                 if position == 0: # Position must be empty
                     for num in range(1,10): # Tries all numbers from 1 to 9
                         if self.__possible(xpos, ypos, num): # Check if the number is a possible
                             self.grid[ypos][xpos] = num # Puts possible number in empty space
-                            ################################################################################################################################################################
+                            # DISPLAY NUMBER LOGIC ##############
                             self.__solve_grid() # Keeps solving
-                            ################################################################################################################################################################
                             self.grid[ypos][xpos] = 0 # If program reaches here, no further numbers can be put into the grid and the square is reset
+                            # DISPLAY NUMBER LOGIC ##############
                     
                     return False # No possible solution has been found for an empty position; exits functions
 
         # If program reaches this point, there are no more empty spaces in the grid and a solution has been found
-
         deepcopy_grid = copy.deepcopy(self.grid) # A copy of the original grid is made
-        self.solutions.append(deepcopy_grid)
+        self.solutions.append(deepcopy_grid) # Solution added to list of solutions
     
     def __possible(self, x, y, n):
         '''Returns True or False if a number can fit in a specific position in the grid 
@@ -208,11 +198,34 @@ class SudokuGrid():
             for row in grid:
                 print(row)
 
-    def __testing(self): ################################################################################################################################################################
-        'Temporary testing function for button'
+    def __start(self): ################################################################################################################################################################
+        'Begins the dynamic solving of the grid'
 
-        self.__solve_grid() 
-        self.__display_solutions()
+        self.grid = [ # Grid representing the 8x8 sudoku grid which is initially empty
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+        # Places each user-entered number into self.grid
+        for ypos, row in enumerate(self.grid): # Goes through each row in the grid
+            for xpos, position in enumerate(row): # Goes through each position in the row
+                    grid_number = self.canvas.find_withtag((ypos,xpos),) # Gets the grid number object with tag at respective position (row, column)
+                    value = self.canvas.itemcget(grid_number, 'text') # Gets the value of the specific grid number
+
+                    if value: # If the cell is filled in
+                        self.grid[ypos][xpos] = int(value)
+                    else: # If the cell is empty
+                        self.grid[ypos][xpos] = 0
+
+        self.__solve_grid() # Solves the filled grid
+        self.__display_solutions() # Displays all solutions
             
 root = tkinter.Tk() # Defines the main window
 root.title('Testing Grid')
